dacfc7191d75c4e3268567d26dc241964740cfcd
Clara Yip <clara.yip1@gmail.com>
2019-07-02 22:03:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,9 +43,8 @@ def reduce_to_total(source_array, starting_point = 0)
 end 
 
 def reduce_to_all_true(source_array)
-  i = 0 
   reduction = true 
-  while i < source_array.length do
+  source_array.each do |number|
     if source_array[i]
     reduction = true
     else 
